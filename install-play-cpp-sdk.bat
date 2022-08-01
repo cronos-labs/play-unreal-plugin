b93943c55b19059e680dd06215d8a13ee2c39d84
Damon Chan <92778185+damoncro@users.noreply.github.com>
2022-08-02 00:41:46 +0800

    Problem: Missing windows native method to prepare the plugin (fix #99) (#100)
    * Problem: Missing windows native method to prepare the plugin (fix #99)
    
    Solution:
    - Add install-play-cpp-sdk.bat
    
    * Use v0.0.3-alpha

---
+++ install-play-cpp-sdk.bat
@@ -0,0 +1,19 @@
+set PLAYCPPSDK=v0.0.3-alpha
+set WINDOWSSRC=https://github.com/cronos-labs/play-cpp-sdk/releases/download/%PLAYCPPSDK%/play_cpp_sdk_Windows_x86_64.zip
+set WINDOWSHASH="9ad8bedaaa96e8a5898126d40da527a4180b8c7cfe8ab7a2d6d01445ad476efd play_cpp_sdk_Windows_x86_64.zip"
+
+rmdir install\windows\ /s /q
+rmdir ThirdParty\cronosplay\lib\Win64\ /s /q
+
+mkdir -p install\windows
+mkdir -p ThirdParty\cronosplay\lib\Win64
+
+cd install\windows
+powershell -Command "Invoke-WebRequest %WINDOWSSRC% -OutFile play_cpp_sdk_Windows_x86_64.zip"
+
+echo %WINDOWSHASH% | sha256sum -c  -
+unzip play_cpp_sdk_Windows_x86_64.zip
+
+Xcopy /E/I sdk\lib ..\..\ThirdParty\cronosplay\lib\Win64
+
+cd ..\..
