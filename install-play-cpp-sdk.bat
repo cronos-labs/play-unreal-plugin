c74f26768262415d9e0605fca8be13ae389ce7cf
Damon Chen <damon.chen@crypto.com>
2022-08-08 17:10:02 +0800

    Update install-play-cpp-sdk.bat

--- install-play-cpp-sdk.bat
+++ install-play-cpp-sdk.bat
@@ -3,10 +3,10 @@ set WINDOWSSRC=https://github.com/cronos-labs/play-cpp-sdk/releases/download/%PL
 set WINDOWSHASH="9ad8bedaaa96e8a5898126d40da527a4180b8c7cfe8ab7a2d6d01445ad476efd play_cpp_sdk_Windows_x86_64.zip"
 
 rmdir install\windows\ /s /q
-rmdir ThirdParty\cronosplay\lib\Win64\ /s /q
+rmdir Source\ThirdParty\cronosplay\lib\Win64\ /s /q
 
 mkdir -p install\windows
-mkdir -p ThirdParty\cronosplay\lib\Win64
+mkdir -p Source\ThirdParty\cronosplay\lib\Win64
 
 cd install\windows
 powershell -Command "Invoke-WebRequest %WINDOWSSRC% -OutFile play_cpp_sdk_Windows_x86_64.zip"
@@ -14,6 +14,6 @@ powershell -Command "Invoke-WebRequest %WINDOWSSRC% -OutFile play_cpp_sdk_Window
 echo %WINDOWSHASH% | sha256sum -c  -
 unzip play_cpp_sdk_Windows_x86_64.zip
 
-Xcopy /E/I sdk\lib ..\..\ThirdParty\cronosplay\lib\Win64
+Xcopy /E/I sdk\lib ..\..\Source\ThirdParty\cronosplay\lib\Win64
 
 cd ..\..
