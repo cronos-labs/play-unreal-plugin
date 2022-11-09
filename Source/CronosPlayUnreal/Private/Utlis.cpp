825f8ab93b17772a4b6f78fa5642bf94ce4b905f
Damon Chen <damon.chen@crypto.com>
2022-11-09 12:12:38 +0800

    Problem: Missing chain_id for SignEip155Transaction (fix #199)
    Close:
    - #199
    
    Solutions:
    - Add chain_id to function: SignEip155Transaction
    - Rename FInitializeWalletConnectBlockingDelegate to FInitializeWalletConnectDelegate
    - Rename EnsureSessionBlocking to EnsureSession
    - Add GetCryptoWalletUrl
    - Turn SignPersonalBlocking into async SignPersonal
    - Rename SignEip155TransactionBlocking to SignEip155Transaction
    - Add Utils
      - Add ToHex function
    - Fix docs

---
+++ Source/CronosPlayUnreal/Private/Utlis.cpp
@@ -0,0 +1,8 @@
+// Copyright 2022, Cronos Labs. All Rights Reserved
+
+#include "Utlis.h"
+#include "Containers/UnrealString.h"
+
+FString UUtlis::ToHex(TArray<uint8> address) {
+  return BytesToHex(address.GetData(), address.Num());
+}
---
+++ Source/CronosPlayUnreal/Public/Utlis.h
@@ -0,0 +1,27 @@
+// Copyright 2022, Cronos Labs. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Utlis.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class CRONOSPLAYUNREAL_API UUtlis : public UBlueprintFunctionLibrary {
+  GENERATED_BODY()
+
+public:
+  /**
+   * Convert bytes to hex
+   * @param address bytes address
+   * @return address in hex string
+   *
+   */
+  UFUNCTION(BlueprintCallable,
+            meta = (DisplayName = "ToHex", Keywords = "PlayCppSdk"),
+            Category = "Utils")
+  static FString ToHex(TArray<uint8> address);
+};
